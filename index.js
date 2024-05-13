7209c2ea4de8adc151d51d12e6623b681d127158
Alex Mai <maitrongtuanbmt@gmail.com>
2024-05-13 05:43:16 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+var multer = require('multer')
+const upload = multer()
 var app = express();
 
 app.use(cors());
@@ -11,7 +12,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function(req,res){
+  res.json({
+    name:req.file.filename,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
